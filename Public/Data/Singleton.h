64c1c4b1d3f8c2952483e3d44e2e2e7fc48c0262
BoomBac <2229245706@qq.com>
2021-04-24 20:26:14 +0800

    2次遗漏

---
+++ Public/Data/Singleton.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+template<class T>
+class SIMDARKSOULS_API Singleton
+{
+public:
+ static TSharedPtr<T> Get();
+private:
+ static void Initialize();
+ static TSharedPtr<T> Create();
+
+ static TSharedPtr<T> TInstance;
+};
+template<class T>
+TSharedPtr<T> Singleton<T>::TInstance = nullptr;
+
+template <class T>
+TSharedPtr<T> Singleton<T>::Get()
+{
+   Initialize();
+   return TInstance;
+}
+
+template <class T>
+void Singleton<T>::Initialize()
+{
+ if(!TInstance.IsValid())
+   TInstance = Create();
+}
+
+template <class T>
+TSharedPtr<T> Singleton<T>::Create()
+{
+  TSharedRef<T> Ref = MakeShareable(new T());
+  return Ref;
+}
